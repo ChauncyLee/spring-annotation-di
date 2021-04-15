3e9e2c41c1316697ec8692f806f9f7d5f0408382
lcq <chuncyli@163.com>
2021-04-15 19:53:43 +0800

    :new: 测试component容器创建对象

---
+++ src/main/java/com/hz/spring/an01/Student.java
@@ -0,0 +1,47 @@
+package com.hz.spring.an01;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Component:创建对象的，等同于<bean></bean>的功能
+ *      属性：value 就是对象的名称，也就是bean的id值
+ *          value的值是唯一的，创建的对象在整个spring容器中就一个
+ *      位置：在类的上面
+ *
+ * @Component(value = "myStudent")等同于
+ * <bean id="myStudent" class="com.hz.spring.an01.Student"/>
+ */
+@Component(value = "myStudent")
+public class Student {
+    private String name;
+    private Integer age;
+
+    public Student(){
+        System.out.println("==Student无参数构造方法==");
+    }
+
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
